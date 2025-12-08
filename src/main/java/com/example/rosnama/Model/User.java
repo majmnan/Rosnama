3e0c5c05b17c851044e59c0d0a2b3178a4019244
abosaad <ibra9v1221@gmail.com>
2025-12-08 21:41:32 +0300

    ibrahimcoom

---
+++ src/main/java/com/example/rosnama/Model/User.java
@@ -0,0 +1,21 @@
+package com.example.rosnama.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
