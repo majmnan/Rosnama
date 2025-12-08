b4a7956f2524a65cbb7c5548b08670b9594b721e
majmnan <majmnan@gmail.com>
2025-12-09 00:06:34 +0300

    commit

--- src/main/java/com/example/rosnama/controller/UserController.java
+++ src/main/java/com/example/rosnama/controller/UserController.java
@@ -1,4 +1,43 @@
-package com.example.rosnama.controller;
+package com.example.rosnama.Controller;
 
+import com.example.rosnama.Api.ApiResponse;
+import com.example.rosnama.Model.User;
+import com.example.rosnama.Repository.UserRepository;
+import com.example.rosnama.Service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/get")
+    public ResponseEntity<List<User> >getAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse>addUser(@RequestBody @Valid User user ) {
+        userService.addUser(user);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("User Added successfully"));
+
+    }
+    @PutMapping("/update{id}")
+    public ResponseEntity<ApiResponse>updateUser(@PathVariable Integer id ,@RequestBody @Valid User user){
+        userService.updateUser(id, user);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("User Updated Successfully"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse>deleteUser(@PathVariable Integer id ){
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("User Deleted Successfully"));
+    }
 }
