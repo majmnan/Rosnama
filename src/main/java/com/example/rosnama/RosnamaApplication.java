ac43e700d519885e932661dc2b5cd9d9410c777c
majmnan <majmnan@gmail.com>
2025-12-08 21:31:02 +0300

    commit

---
+++ src/main/java/com/example/rosnama/RosnamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rosnama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosnamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RosnamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rosnama/RosnamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rosnama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosnamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
