a24d1b40a73add73befd4f0839cba60014b0c626
majmnan <majmnan@gmail.com>
2025-12-09 00:06:34 +0300

    commit

--- src/main/java/com/example/rosnama/service/UserService.java
+++ src/main/java/com/example/rosnama/service/UserService.java
@@ -1,8 +1,8 @@
-package com.example.rosnama.service;
+package com.example.rosnama.Service;
 
-import com.example.rosnama.apiresponse.ApiException;
-import com.example.rosnama.model.User;
-import com.example.rosnama.repository.UserRepository;
+import com.example.rosnama.Api.ApiException;
+import com.example.rosnama.Model.User;
+import com.example.rosnama.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,37 +16,38 @@ public class UserService {
 
 
     public List<User>getAllUsers(){
-       return userRepository.findAll();
+        return userRepository.findAll();
     }
 
 
     public void addUser(User user){
-        User user1 = userRepository.findUserByEmail(user.getEmail());
-        User user2 = userRepository.findUserByUsername(user.getUsername());
 
-       if(user1 !=null){
-           throw new ApiException("Email Already exsit");
-       }
-
-       if(user2 != null){
-           throw new ApiException("UserName Already exsit");
-       }
-
-       userRepository.save(user);
+        userRepository.save(user);
 
     }
 
 
     public void updateUser(Integer id , User user){
 
-        User user1 = userRepository.findUserById(id);
-       List <User> users =  userRepository.findAll();
+        User old = userRepository.findUserById(id);
 
-        if(user1 == null){
+        if(old == null){
             throw new ApiException("User not Exists");
         }
+        old.setAge(user.getAge());
+        old.setEmail(user.getEmail());
+        old.setPassword(user.getPassword());
+        old.setPhoneNumber(user.getPhoneNumber());
+        old.setUsername(user.getUsername());
+        userRepository.save(old);
 
+    }
 
-
+    public void deleteUser(Integer id) {
+        User user =   userRepository.findUserById(id);
+        if(user == null){
+            throw new ApiException("User not found");
+        }
+        userRepository.delete(user);
     }
 }
