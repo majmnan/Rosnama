e35d946c11cc0be4e9e422c1dedf0c5bde8f5456
majmnan <majmnan@gmail.com>
2025-12-09 00:31:10 +0300

    commit

--- src/main/java/com/example/rosnama/repository/UserRepository.java
+++ src/main/java/com/example/rosnama/repository/UserRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findUserById(Integer id);
-
-    User findUserByEmail(String email);
-
-    User findUserByUsername(String username);
 }
