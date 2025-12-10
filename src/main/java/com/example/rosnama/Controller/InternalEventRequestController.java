a930e9dd90a102b6632116cb85d0b8c2dddb4156
abosaad <ibra9v1221@gmail.com>
2025-12-10 13:43:19 +0300

    commit-v9

---
+++ src/main/java/com/example/rosnama/Controller/InternalEventRequestController.java
@@ -0,0 +1,53 @@
+package com.example.rosnama.Controller;
+
+import com.example.rosnama.Api.ApiResponse;
+import com.example.rosnama.Model.InternalEventRequest;
+import com.example.rosnama.Service.InternalEventRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class InternalEventRequestController {
+
+    private final InternalEventRequestService internalEventRequestService;
+
+    @GetMapping("/get/{admin_id}")
+    public ResponseEntity<List <InternalEventRequest>>getInternalEventRequests(@PathVariable Integer admin_id){
+        return ResponseEntity.status(HttpStatus.OK).body(internalEventRequestService.getAllEventsRequest(admin_id));
+
+    }
+
+
+    @PutMapping ("/offer-for/{adminId}/{requestId}/{price}")
+    public ResponseEntity<ApiResponse>offerPrice(@PathVariable Integer adminId, @PathVariable Integer requestId,@PathVariable Double price){
+        internalEventRequestService.offerPrice(adminId,requestId,price);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("offer send successfully"));
+
+    }
+
+
+    @PutMapping ("/negotiates/{ownerId}/{requestId}/{price}")
+    public ResponseEntity<ApiResponse>negotiates(@PathVariable Integer ownerId, @PathVariable Integer requestId, @PathVariable Double price){
+        internalEventRequestService.negotiates(ownerId,requestId,price);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("negotiate successfully"));
+
+    }
+
+
+    @PutMapping ("/accept-pay/{ownerId}/{requestId}/{price}")
+    public ResponseEntity<ApiResponse>acceptOfferAndPay(@PathVariable Integer ownerId, @PathVariable Integer requestId){
+        internalEventRequestService.acceptOfferAndPay(ownerId,requestId);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("event accepted and published successfully "));
+
+    }
+
+
+
+
+}
