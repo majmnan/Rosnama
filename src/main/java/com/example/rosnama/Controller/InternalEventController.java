4c6406313c539d03b2c587ce3f7b4f813913c4cb
abosaad <ibra9v1221@gmail.com>
2025-12-10 14:17:35 +0300

    commit-v11

--- src/main/java/com/example/rosnama/Controller/InternalEventController.java
+++ src/main/java/com/example/rosnama/Controller/InternalEventController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/internalevent")
+@RequestMapping("/api/v1/internal-event")
 @RequiredArgsConstructor
 public class InternalEventController {
 
